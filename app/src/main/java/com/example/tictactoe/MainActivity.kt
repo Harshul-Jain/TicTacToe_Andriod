935df9f5b8e0a120a8165bf346d7b3fc1dc746b1
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-07-11 11:10:37 +0530

    Implemented Winning Logics

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -84,10 +84,70 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         if (TURN_COUNT == 9) {
             updateDisplay("Game Draw")
         }
+        checkWinner()
+    }
+
+    private fun checkWinner() {
+        //horizontal
+        for (i in 0..2) {
+            if (boardStatus[i][0] == boardStatus[i][1] && boardStatus[i][0] == boardStatus[i][2]) {
+                if (boardStatus[i][0] == 1) {
+                    updateDisplay("Player X Winner")
+                    return
+                } else if (boardStatus[i][0] == 0) {
+                    updateDisplay("Player O Winner")
+                    return
+                }
+            }
+        }
+        //Vertical
+        for (i in 0..2) {
+            if (boardStatus[0][i] == boardStatus[1][i] && boardStatus[0][i] == boardStatus[2][i]) {
+                if (boardStatus[0][i] == 1) {
+                    updateDisplay("Player X Winner")
+                    return
+                } else if (boardStatus[0][i] == 0) {
+                    updateDisplay("Player O Winner")
+                    return
+                }
+            }
+        }
+        //Diagonal1
+        if (boardStatus[0][0] == boardStatus[1][1] && boardStatus[0][0] == boardStatus[2][2]) {
+            if (boardStatus[0][0] == 1) {
+                updateDisplay("Player X Winner")
+                return
+            } else if (boardStatus[0][0] == 0) {
+                updateDisplay("Player O Winner")
+                return
+            }
+        }
+        //Diagonal2
+        if (boardStatus[0][2] == boardStatus[1][1] && boardStatus[0][2] == boardStatus[2][0]) {
+            if (boardStatus[0][2] == 1) {
+                updateDisplay("Player X Winner")
+                return
+            } else if (boardStatus[0][2] == 0) {
+                updateDisplay("Player O Winner")
+                return
+            }
+        }
+
     }
 
     private fun updateDisplay(text: String) {
         displayTv.text = text
+        if (text.contains("Winner")) {
+            disableAllButtons()
+        }
+    }
+
+    private fun disableAllButtons() {
+        for (i in board) {
+            for (button in i) {
+                button.isEnabled = false
+            }
+        }
     }
 
     private fun updateValue(row: Int, col: Int, player: Boolean) {
